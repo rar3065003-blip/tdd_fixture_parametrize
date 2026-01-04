33ad7d167bbec94419c23d70236bf66e2b52d94c
Андрей Атаманов <rar3065003@gmail.com>
2026-01-04 18:28:42 +0700

    add branch and commit in remote repository

--- test_tdd.py
+++ test_tdd.py
@@ -14,3 +14,14 @@ def test_calculate_tax():
     result = calculate_tax(100.0, 100.0)
     assert result == "Неверный налоговый процент"
 
+    result = calculate_tax("100.0", 10.0)
+    assert result == "Неверный формат данных цены"
+
+    result = calculate_tax(100.0, "10.0")
+    assert result == "Неверный формат данных налогового процента"
+
+    result = calculate_tax(100.0, [10.0])
+    assert result == "Неверный формат данных налогового процента"
+
+    result = calculate_tax([100.0], 10.0)
+    assert result == "Неверный формат данных цены"
